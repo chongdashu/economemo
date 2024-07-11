547a3e06f093852c9294bacc44c0be44c4ea5ef0
Chong-U Lim <me@chongdashu.com>
2024-07-11 15:42:50 +0100

    Run with SSL

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -4,4 +4,4 @@
 source "$(dirname "$0")/run_common.sh"
 
 # Run the FastAPI server with Uvicorn
-uvicorn app.main:app --reload
+uvicorn app.main:app --reload --ssl-keyfile=key.pem --ssl-certfile=cert.pem
