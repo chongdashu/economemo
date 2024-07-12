5fed6aa61b808728fd4e8b3b596a2e69746b4caf
Chong-U Lim <me@chongdashu.com>
2024-07-12 13:53:40 +0100

    Fix local running

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -3,5 +3,8 @@
 # Source the common setup script
 source "$(dirname "$0")/run_common.sh"
 
+# When running locally, use a test db so that it doesn't clobber main db
+export POSTGRES_DB=test_economemo_db
+
 # Run the FastAPI server with Uvicorn
 pytest
