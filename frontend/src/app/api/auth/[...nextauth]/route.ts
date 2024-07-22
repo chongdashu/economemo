310cfeb98b62065b520f3f17a9ea4f6f57df7f0c
Chong-U Lim <me@chongdashu.com>
2024-07-22 22:24:29 +0100

    [frontend] fix compile

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
+import React from 'react';
 import NextAuth from 'next-auth';
 import { authOptions } from './options';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+export const POST = handler;
