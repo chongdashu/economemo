5c8b1189fee96a18dfb2472243e771db29689310
Chong-U Lim <me@chongdashu.com>
2024-08-04 06:27:15 +0100

    [backend] Allow empty pw

--- backend/app/db.py
+++ backend/app/db.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 load_dotenv()
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
