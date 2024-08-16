aff383aca1906d8ec4fe9894b0a9eab5d3c2d18a
Chong-U Lim <me@chongdashu.com>
2024-08-16 14:40:47 +0100

    [chrome-extension] Add support for more sites correctly

--- chrome-extension/src/constants.js
+++ chrome-extension/src/constants.js
@@ -1,16 +1,52 @@
 var WHITELISTED_SITES = [
-    { domain: 'news.yahoo.com', articlePattern: /^https:\/\/news\.yahoo\.com\/.*\.html$/ },
-    { domain: 'cnn.com', articlePattern: /^https:\/\/(edition\.)?cnn\.com\/\d{4}\/\d{2}\/\d{2}\/.*\/index\.html$/ },
-    { domain: 'nytimes.com', articlePattern: /^https:\/\/www\.nytimes\.com\/\d{4}\/\d{2}\/\d{2}\/.*\.html$/ },
-    { domain: 'foxnews.com', articlePattern: /^https:\/\/www\.foxnews\.com\/.*$/ },
-    { domain: 'bbc.com', articlePattern: /^https:\/\/www\.bbc\.com\/news\/.*-\d+$/ },
-    { domain: 'bbc.co.uk', articlePattern: /^https:\/\/www\.bbc\.co\.uk\/news\/.*-\d+$/ },
-    { domain: 'msn.com', articlePattern: /^https:\/\/www\.msn\.com\/.*\/ar-.*$/ },
-    { domain: 'reuters.com', articlePattern: /^https:\/\/www\.reuters\.com\/.*\/.*-.*\/$/ },
-    { domain: 'wsj.com', articlePattern: /^https:\/\/www\.wsj\.com\/articles\/.*$/ },
-    { domain: 'nbcnews.com', articlePattern: /^https:\/\/www\.nbcnews\.com\/.*$/ },
-    { domain: 'news.google.com', articlePattern: /^https:\/\/news\.google\.com\/articles\/.*$/ },
-    { domain: 'economist.com', articlePattern: /^https:\/\/www\.economist\.com\/[a-z-]+\/\d{4}\/\d{2}\/\d{2}\/[a-z0-9-]+$/ },
+    { 
+      domain: 'yahoo.com', 
+      articlePattern: /^https:\/\/(www\.)?(yahoo\.com\/news\/|news\.yahoo\.com\/).*(-\d+\.html|\.[a-zA-Z]+)$/
+    },
+    { 
+      domain: 'cnn.com', 
+      articlePattern: /^https:\/\/(edition\.)?cnn\.com\/\d{4}\/\d{2}\/\d{2}\/.*\/index\.html$/
+    },
+    { 
+      domain: 'nytimes.com', 
+      articlePattern: /^https:\/\/www\.nytimes\.com\/\d{4}\/\d{2}\/\d{2}\/.*\.html$/
+    },
+    { 
+      domain: 'foxnews.com', 
+      articlePattern: /^https:\/\/www\.foxnews\.com\/.*$/
+    },
+    { 
+      domain: 'bbc.com', 
+      articlePattern: /^https:\/\/www\.bbc\.com\/(news|sport)\/(\w+\/)*\w+-\d+$/
+    },
+    { 
+      domain: 'bbc.co.uk', 
+      articlePattern: /^https:\/\/www\.bbc\.co\.uk\/(news|sport)\/(articles|live)\/[a-z0-9]+$/
+    },
+    { 
+      domain: 'msn.com', 
+      articlePattern: /^https:\/\/www\.msn\.com\/.*\/ar-.*$/
+    },
+    { 
+      domain: 'reuters.com', 
+      articlePattern: /^https:\/\/www\.reuters\.com\/.*\/.*-.*\/$/
+    },
+    { 
+      domain: 'wsj.com', 
+      articlePattern: /^https:\/\/www\.wsj\.com\/articles\/.*$/
+    },
+    { 
+      domain: 'nbcnews.com', 
+      articlePattern: /^https:\/\/www\.nbcnews\.com\/.*$/
+    },
+    { 
+      domain: 'news.google.com', 
+      articlePattern: /^https:\/\/news\.google\.com\/articles\/.*$/
+    },
+    { 
+      domain: 'economist.com', 
+      articlePattern: /^https:\/\/www\.economist\.com\/[a-z-]+\/\d{4}\/\d{2}\/\d{2}\/[a-z0-9-]+$/
+    },
   ];
   
   export function isWhitelistedArticlePage(url) {
